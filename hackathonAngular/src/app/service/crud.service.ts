c1dea2afe4a007734feaaef0093824d875d960df
AresianGTI <onobenik@freenet.de>
2021-02-12 21:31:14 +0100

    Da hast du

---
+++ hackathonAngular/src/app/service/crud.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+
+  constructor(public fireservices:AngularFirestore) {
+
+   }
+
+  public create_NewAzubi(AzubiX){
+      return this.fireservices.collection('Azubi').add(AzubiX);
+  }
+
+  public getAllAzubis() {
+
+
+  }
+
+}
